7e19c5d6d79adbd1158e9062ea92c091e55fcab4
Katie Knutson <katieknutson@Katies-MacBook-Pro.local>
2015-03-29 17:14:32 -0500

    recursion

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,53 +1,160 @@
 
 public class BinaryTree 
 {
-	private Node2 root;
+	//private Node2 root;
+	private boolean isEmpty;
+	private int payload;
+	private BinaryTree leftTree;
+	private BinaryTree rightTree;
 	
 	public BinaryTree()
 	{
-		this.root = null;
+		this.isEmpty = true;
+		this.leftTree = null;
+		this.rightTree = null;
 	}
 
+	public boolean search(int value)
+	{
+		if(isEmpty)
+		{
+			return false;
+		}
+		else
+		{
+			if(value == this.payload)
+			{
+				return true;
+			}
+			else if (value < this.payload)
+			{
+				if(this.leftTree == null)
+				{
+					return false;
+				}
+				else
+				{
+					return this.leftTree.search(value);
+				}
+			}
+			else if (value > this.payload)
+			{
+				if(this.rightTree == null)
+				{
+					return false;
+				}
+				else
+				{
+					return this.rightTree.search(value);
+				}
+			}
+		}
+		return false;
+	}
+	
+	private void visitInOrder()
+	{
+		if(this.leftTree != null)
+		{
+			this.leftTree.visitInOrder();
+		}
+		System.out.println(this.payload);
+		if(this.rightTree != null)
+		{
+			this.rightTree.visitInOrder();
+		}
+	}
+	
 	public void displayInOrder()
 	{
-		//System.out.println("**** In Order ****");
-		if(this.root == null)
+		System.out.println("******In Order******");
+		if(this.isEmpty)
 		{
-			//System.out.println("Empty Tree");
+			System.out.println("Empty Tree");
 		}
 		else
 		{
-			displayInOrder(Node2.getLeft());
-			System.out.println(Node2.addNode());
-			displayInOrder(Node2.getRight());
+			this.visitInOrder();
 		}
 	}
 
-	public void displayPostOrder()
+	private void visitPreOrder()
 	{
-		//System.out.println("**** Post Order ****");
-		if(this.root == null)
+		System.out.println(this.payload);
+		if(this.leftTree != null)
 		{
-			//System.out.println("Empty Tree");
+			this.leftTree.visitPreOrder();
+		}
+		if(this.rightTree != null)
+		{
+			this.rightTree.visitPreOrder();
+		}
+	}
+
+	public void displayPreOrder()
+	{
+		System.out.println("******Pre Order******");
+		if(this.isEmpty)
+		{
+			System.out.println("Empty Tree");
 		}
 		else
 		{
-			displayInOrder(Node2.getLeft());
-			displayInOrder(Node2.getRight());
-			System.out.println(Node2.addNode());
+			this.visitPreOrder();
+		}
+	}
+
+	private void visitPostOrder()
+	{
+		if(this.leftTree != null)
+		{
+			this.leftTree.visitPostOrder();
+		}
+		if(this.rightTree != null)
+		{
+			this.rightTree.visitPostOrder();
+		}
+		System.out.println(this.payload);
+	}
+	
+	public void displayPostOrder()
+	{
+		System.out.println("******Post Order******");
+		if(this.isEmpty)
+		{
+			System.out.println("Empty Tree");
+		}
+		else
+		{
+			this.visitPostOrder();
 		}
 	}
 
 	public void add(int value)
 	{
-		Node2 theNode = new Node2(value);
-		if(this.root == null)
+		if(this.isEmpty)
 		{
-			this.root = theNode;
+			this.payload = value;
+			this.isEmpty = false;
 		}
 		else
 		{
-			this.root.addNode2(theNode);
+			if(value <= this.payload)
+			{
+				if(this.leftTree == null)
+				{
+					this.leftTree = new BinaryTree();
+				}
+				this.leftTree.add(value);
+			}
+			else
+			{
+				if(this.rightTree == null)
+				{
+					this.rightTree = new BinaryTree();
+				}
+				this.rightTree.add(value);
+			}
 		}
 	}
 }
