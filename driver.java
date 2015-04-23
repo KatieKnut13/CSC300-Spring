e605f57051ad01fa8dda7e90546f61bf9eb0a9c8
Katie Knutson <katieknutson@Katies-MacBook-Pro.local>
2015-04-23 10:06:11 -0500

    added

--- driver.java
+++ driver.java
@@ -1,13 +1,12 @@
-import java.util.Random;
-import java.util.Scanner;
 
 public class driver 
 {
-
 	public static void main(String[] args) 
 	{
-		RPCalc theCalc = new RPCalc();
-		theCalc.on();
+		Parser p = new Parser("   a =    (a*  b) - c;");
+		Parser p2 = new Parser("    a = b - c;");
+		
+		p.parse();
 	}
 
 }
